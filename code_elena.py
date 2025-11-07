e2967fa7c1d9b04ce7530b63da0db0b780a2c5fe
AutumnBag153338 <davperdel07@gmail.com>
2025-11-08 00:13:22 +0100

    el code de la tonota esta

---
+++ code_elena.py
@@ -0,0 +1,8 @@
+animal = input("Introduzca al primer animal: ")
+animal2 = input("Introduzca al segundo animal: ")
+animal3 = input("Introduzca al tercer animal: ")
+animal4 = input("Introduzca al cuarto animal: ")
+animal5 = input("Introduzca al quinto animal: ")
+
+zorro = " "
+print("Su combinación animalil es: " + animal + zorro + animal2 + zorro + animal3 + zorro + animal4 + zorro + animal5)
